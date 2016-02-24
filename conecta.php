463eb8d9614825d33006ee4d1d35709d4c67c4d2
gustavoesa <gustavopacoca@hotmail.com>
2016-02-24 01:41:11 -0300

    Added files via upload

--- conecta.php
+++ conecta.php
@@ -3,10 +3,10 @@
 		ini_set(“display_errors”, 0 );
 		echo "<html xmlns=http://www.w3.org/1999/xhtml lang=pt-br xml:lang=pt-br>";
 		echo "<meta http-equiv=Content-Type content=text/html; charset=utf-8 />";
-		$host = "localhost";
+		$host = "127.10.163.130";
 		$banco = "valemobi";
-		$user = "root";
-		$senha = "";
+		$user = "adminLzGE3k9";
+		$senha = "Q2g3ceTmDe63";
 
 		$con = mysql_connect($host, $user, $senha) or die("Erro conexão");
 
